ddca7da2f9a53032334ac6a89cc8235ac2f8aabf
Oscar Alonso Rueda Ruiz <orueda@sancsoft.com>
2023-10-04 19:53:52 -0500

    added guard for navigations

--- basic-spa-framework/src/app/app-routing.module.ts
+++ basic-spa-framework/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'app',
+    canActivate: [ AuthGuard ],
     loadChildren: () => import('./layout/layout.module').then((m) => m.LayoutModule),
   },
   {
